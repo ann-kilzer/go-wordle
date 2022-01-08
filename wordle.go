8bb4b03ccdd07096c861d1960ee879fd4033dc3d
Ann 杏 Kilzer <akilzer@gmail.com>
2022-01-08 23:23:33 +0900

    first draft of game

---
+++ wordle.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+var word = "BIRDS"
+
+const WORD_LENGTH = 5
+const ROUNDS = 6
+const A = 65
+
+type Round struct {
+	guess    []rune // user input
+	feedback string // the response to the user
+}
+
+type Game struct {
+	reader      *bufio.Reader
+	usedLetters [26]bool
+	rount       int
+	rounds      [ROUNDS]*Round
+}
+
+func main() {
+	fmt.Println("WORDLE")
+	fmt.Println()
+
+	game := NewGame()
+
+	for i := 0; i < ROUNDS; i++ {
+		game.printRow()
+		game.printLetters()
+		game.readGuess()
+	}
+
+}
+
+func NewGame() *Game {
+	return &Game{
+		reader: bufio.NewReader(os.Stdin),
+	}
+}
+
+func (g *Game) printLetters() {
+	// TODO
+	for i := 0; i < len(g.usedLetters); i++ {
+		if g.usedLetters[i] {
+			fmt.Print("x")
+		} else {
+			fmt.Printf("%c", i+A)
+		}
+	}
+
+	fmt.Println()
+}
+
+func (g *Game) printRow() {
+	for i := 0; i < WORD_LENGTH; i++ {
+		fmt.Print("[ ]")
+		// TODO:
+		// [ ] unfound
+		// [?] wrong location
+		// [*] found
+	}
+
+	fmt.Println()
+}
+
+func (g *Game) readGuess() error {
+	fmt.Print(">")
+
+	var b bytes.Buffer
+
+	for i := 0; i < WORD_LENGTH; i++ {
+		r, err := g.readLetter()
+		if err != nil {
+			return err
+		}
+
+		b.WriteRune(r)
+	}
+
+	fmt.Println(b.String())
+	// TODO save guess
+
+	return nil
+}
+
+func (g *Game) readLetter() (rune, error) {
+	char, _, err := g.reader.ReadRune()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return char, err
+}
