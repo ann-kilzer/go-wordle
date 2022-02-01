2e4f72ecc69fa036e2869ad98143ad35094584c3
Ann 杏 Kilzer <akilzer@gmail.com>
2022-02-01 23:18:11 +0900

    trie and test

--- src/trie/trie.go
+++ src/trie/trie.go
@@ -1,5 +1,11 @@
 package trie
 
+import (
+	"strings"
+)
+
+const UPPER_A = 65
+
 const ALPHABET_LEN = 26
 const START_CHAR = '*'
 
@@ -17,8 +23,42 @@ type Trie struct {
 func NewTrie() *Trie {
 	return &Trie{
 		root: &Node{
-			letter: START_CHAR,
 			wordEnd: false,
 		},
 	}
+}
+
+// Insert adds the word to the trie
+
+func (t *Trie) Insert(word string) {
+	w := strings.ToUpper(word)
+	var prev *Node
+	cur := t.root
+	for i := 0; i < len(w); i++ {
+		letter := w[i]
+		index := letter - UPPER_A
+		if cur.next[index] == nil {
+			cur.next[index] = &Node{letter: letter}
+		}
+		prev = cur
+		cur = cur.next[index]
+	}
+	prev.wordEnd = true
+}
+
+func (t *Trie) HasWord(word string) bool {
+	w := strings.ToUpper(word)
+	var prev *Node
+	cur := t.root
+	for i := 0; i < len(w); i++ {
+		letter := w[i]
+		index := letter - UPPER_A
+		if cur.next[index] == nil {
+			return false
+		}
+		prev = cur
+		cur = cur.next[index]
+	}
+
+	return prev != nil && prev.wordEnd 
 }
